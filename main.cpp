60dba5dcf79f22c0da7a2a5c3e5947958c37a70b
Andrew St. Pierre <astpier@purdue.edu>
2019-01-17 14:40:11 -0500

    added a timing tool to check runtime

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "Player.h"
 #include <iostream>
+#include <ctime>
 
 // Function prototypes
 std::string truth(int binaryVal);
 
 // Main function [point of entry]
 int main(int argc, char * argv[]) {
+    int start = clock();
+
     // Create Player objects and assign addresses to pointers
     // Note: we are allocating the objects on the heap (keyword: new)
     Player * andrew = new Player("Andy",0);
@@ -24,6 +27,8 @@ int main(int argc, char * argv[]) {
     std::cout<<"Name: "<<andrew->getName()<<" Score: "<<andrew->getScore()<<" Win yet? "<<truth(andrew->isWinner())<<std::endl;
     std::cout<<"Name: "<<jeff->getName()<<" Score: "<<jeff->getScore()<<" Win yet? "<<truth(jeff->isWinner())<<std::endl;
 
+    int stop=clock();
+    std::cout << "time running: "<<(stop-start)/double(CLOCKS_PER_SEC)*1000 <<"ms"<< std::endl;
     // Return success
     return 0;
 }
