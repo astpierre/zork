a7648a53ae267a7e5b0caa9584637fb65b71da84
geissjeff <geissjeff15@gmail.com>
2019-03-11 12:55:40 -0400

    Added Container and Item

---
+++ include/Container.h
@@ -0,0 +1,8 @@
+//
+// Created by Jeff on 3/11/2019.
+//
+
+#ifndef ZORK_CONTAINER_H
+#define ZORK_CONTAINER_H
+
+#endif //ZORK_CONTAINER_H
---
+++ include/Item.h
@@ -0,0 +1,13 @@
+//
+// Created by Jeff on 3/11/2019.
+//
+
+#ifndef ZORK_ITEM_H
+#define ZORK_ITEM_H
+#include <vector>
+#include <string>
+class Item : public Component {
+    std::string writing;
+
+};
+#endif //ZORK_ITEM_H
